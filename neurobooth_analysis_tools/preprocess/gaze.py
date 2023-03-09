bbcc12d3d98e3b22b00210d9ce11390aea4edcf4
Brandon Oubre <boubre@mgh.harvard.edu>
2023-03-09 10:48:41 -0500

    Adding additional gaze preprocessing algorithm

--- neurobooth_analysis_tools/preprocess/gaze.py
+++ neurobooth_analysis_tools/preprocess/gaze.py
@@ -17,6 +17,18 @@ def normalize_to_screen(gaze_pos: np.ndarray, screen_width: float, screen_height
     gaze_pos[:, 1] -= screen_height / 2
 
     # Normalize so that the larger dimension is -1 to 1; preserve aspect ratio
-    gaze_pos /= (max(screen_width, screen_height) / 2)
+    norm_factor = max(screen_width, screen_height) / 2
+    gaze_pos /= norm_factor
 
     return gaze_pos
+
+
+def normalize_max_value(gaze_pos: np.ndarray) -> np.ndarray:
+    """
+    Normalize a gaze position time-series so that that maximum absolute value does not exceed 1.
+    If the data are already within this range, no normalization occurs.
+    :return: The normalized gaze position time-series.
+    """
+    norm_factor = np.max(np.abs(gaze_pos))
+    norm_factor = max(norm_factor, 1)
+    return gaze_pos / norm_factor
