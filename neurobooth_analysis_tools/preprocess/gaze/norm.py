Fixing px2dva code, was converting to radians instead of degrees

neurobooth_analysis_tools/preprocess/gaze/norm.py
@@ -69,7 +69,7 @@ def pixel_to_dva(
     gaze_pos[:, 0] = np.arctan(gaze_pos[:, 0] / mm_to_target)
     gaze_pos[:, 1] = np.arctan(gaze_pos[:, 1] / mm_to_target)
 
-    return gaze_pos
+    return np.degrees(gaze_pos)
 
 
 def normalize_dva_to_screen(
@@ -89,7 +89,7 @@ def normalize_dva_to_screen(
     # Normalize so that the larger dimension is -1 to 1; preserve aspect ratio
     norm_factor_px = max(screen.width_px, screen.height_px) / 2
     norm_factor_mm = norm_factor_px * (25.4 / screen.px_per_inch)  # Convert pixels to mm
-    norm_factor = np.arctan(norm_factor_mm / mm_to_target)  # Convert to dva
+    norm_factor = np.degrees(np.arctan(norm_factor_mm / mm_to_target))  # Convert mm to dva
     if not np.isscalar(norm_factor):
         norm_factor = norm_factor[:, np.newaxis]
     return gaze_pos / norm_factor