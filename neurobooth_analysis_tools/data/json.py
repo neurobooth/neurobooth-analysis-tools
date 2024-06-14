e8a4f24bda0cea180290686e93a48366cead7065
Brandon Oubre <boubre@mgh.harvard.edu>
2024-06-14 08:13:54 -0400

    Updating JSON parsing to account for new file format that includes metadata. Maintains compataibility with old function calls and files.

--- neurobooth_analysis_tools/data/json.py
+++ neurobooth_analysis_tools/data/json.py
@@ -11,18 +11,41 @@ from typing import NamedTuple, List, Dict
 from neurobooth_analysis_tools.data.files import FILE_PATH, resolve_filename
 
 
+class IPhoneMetadata(NamedTuple):
+    ios_version: str
+    app_version: str
+    device_id: str
+    device_type: str  # See https://www.theiphonewiki.com/wiki/Models
+
+
 class IPhoneJsonResult(NamedTuple):
+    metadata: IPhoneMetadata | None  # Older JSON files do not contain metadata
     video: pd.DataFrame
     dropped_video: pd.DataFrame
     audio: pd.DataFrame
 
 
 def parse_iphone_json(file: FILE_PATH) -> IPhoneJsonResult:
+    """
+    Parse an iPhone JSON file and return its contests in a structured format.
+    :param file: The file to parse.
+    :return: A structure containing the video and audio synchronization data.
+    """
     with open(resolve_filename(file), 'r') as f:
         data = json.load(f)
+
+    # Handle newer file versions. Older files did not have metadata and are a list at the outermost level.
+    if isinstance(data, dict):
+        metadata = _iphone_json_extract_metadata(data['Metadata'])
+        data = data['Framedata']
+    else:
+        metadata = None
+
+    # Each string in the list can itself be parsed as JSON
     data = list(map(json.loads, data))
 
     return IPhoneJsonResult(
+        metadata=metadata,
         video=_iphone_json_extract_video(data),
         dropped_video=_iphone_json_extract_dropped_video(data),
         audio=_iphone_json_extract_audio(data),
@@ -63,3 +86,12 @@ def _iphone_json_extract_audio(data: List[Dict[str, str]]) -> pd.DataFrame:
     })
 
     return df
+
+
+def _iphone_json_extract_metadata(metadata: Dict[str, str]) -> IPhoneMetadata:
+    return IPhoneMetadata(
+        ios_version=metadata['iOSVersionNumber'],
+        app_version=metadata['appVersionNumber'],
+        device_id=metadata['deviceID'],
+        device_type=metadata['deviceType'],
+    )
